fb27807796a4d32809b3cb5780f035db9f9c9c37
Hardy Milalu <hmilalu@talend.com>
2022-08-19 17:04:37 +0200

    python script

---
+++ firstScript.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#import panda as pd 
+import os
+
+TEST_PATH = r"C:\Users\hmilalu\.spyder-py3\Setup.Lst"
+
+print("stating to read ...")
+
+# =============================================================================
+# with open(TEST_PATH, "r", encoding="utf-8") as f:
+#     for line in f :
+#         print(line)
+#         dir(line)
+# =============================================================================
+
+file = open(TEST_PATH, "r", encoding="utf-8")
+
+lines = file.readlines()
+file.close()
+
+for line in lines :
+         print(line)
+
+
+print("Directory contents:")
+
+for f in os.listdir():
+    print(f)
